1366237abaf992737b24cb102e88f074b39a9734
nandanmathure@gmail.com <nandanmathure@gmail.com>
2020-06-13 13:54:55 -0400

    Created Data Generator

---
+++ src/HumanResource.DataGenerator/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+
+namespace HumanResource.DataGenerator
+{
+    /// <summary>
+    /// Represents command line options for Data Generator
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets/Sets sample size of output file
+        /// </summary>
+        [Option('s', "sampleSize", Required = false, Default = 25, HelpText = "Number of records to be generated in sample")]
+        public int SampleSize { get; set; }
+
+        /// <summary>
+        /// Gets/Sets name of output file
+        /// </summary>
+        [Option('o', "outputFileName", Required = true, HelpText = "Name of output file optionally along with full path")]
+        public string OutputFileName { get; set; }
+    }
+}
