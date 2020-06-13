c432fdf209b6664ed598ef3339351cce725130b8
nandanmathure@gmail.com <nandanmathure@gmail.com>
2020-06-13 13:24:06 -0400

    Added POCO Classes

---
+++ src/HumanResource.DataElements/Gender.cs
@@ -0,0 +1,18 @@
+﻿namespace HumanResource.DataElements
+{
+    /// <summary>
+    /// Enumerates gender of employee
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Male
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// Female
+        /// </summary>
+        Female
+    }
+}
