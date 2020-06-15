a8bc3cf705b110b0045cb642bbd6e17f949b69c4
Nandan Mathure <nandanmathure@gmail.com>
2020-06-15 09:50:15 -0400

    Added seperate strategy for Parallel and Async

--- src/HumanResource.BackgroundWorkerFuncApp/ProcessIncomingFile.cs
+++ src/HumanResource.BackgroundWorkerFuncApp/ProcessIncomingFile.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HumanResource.DataElements;
@@ -14,8 +15,8 @@ namespace HumanResource.BackgroundWorkerFuncApp
 {
     public static class ProcessIncomingFile
     {
-        [FunctionName("ProcessIncomingFile")]
-        public static async Task Run(
+        [FunctionName("ProcessIncomingFileParallelAsync")]
+        public static async Task ParallelAsyncRun(
             [BlobTrigger(
                 "file-drop/{name}",
                 Connection = "AzureWebJobsStorage")]Stream blob,
@@ -23,13 +24,36 @@ namespace HumanResource.BackgroundWorkerFuncApp
                 "requests",
                 Connection = "AzureServiceBus")]IAsyncCollector<Employee> messages,
             [Blob(
-                "logs/{name}",
+                "logs/Parallel{name}",
                 FileAccess.Write,
                 Connection = "AzureWebJobsStorage")]Stream logStream,
             string name,
             ILogger log)
         {
-            /// Record Start time and start the stop watch
+            await CoreProcessing(blob, messages, logStream, name, ParallelAsyncProcessing);
+        }
+
+        [FunctionName("ProcessIncomingFileAsync")]
+        public static async Task AsyncRun(
+            [BlobTrigger(
+                "file-drop/{name}",
+                Connection = "AzureWebJobsStorage")]Stream blob,
+            [ServiceBus(
+                "requests",
+                Connection = "AzureServiceBus")]IAsyncCollector<Employee> messages,
+            [Blob(
+                "logs/Async{name}",
+                FileAccess.Write,
+                Connection = "AzureWebJobsStorage")]Stream logStream,
+            string name,
+            ILogger log)
+        {
+            await CoreProcessing(blob, messages, logStream, name, AsyncProcessing);
+        }
+
+        private static async Task CoreProcessing(Stream blob, IAsyncCollector<Employee> messages, Stream logStream, string name, Action<IAsyncCollector<Employee>, List<Employee>> strategyAction)
+        {
+            // Record Start time and start the stop watch
             var startTime = DateTime.UtcNow;
             var stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -39,7 +63,7 @@ namespace HumanResource.BackgroundWorkerFuncApp
             using (var sr = new StreamReader(blob))
             {
                 using var jtr = new JsonTextReader(sr);
-                JsonSerializer serializer = new JsonSerializer()
+                var serializer = new JsonSerializer()
                 {
                     ContractResolver = new CamelCasePropertyNamesContractResolver()
                 };
@@ -48,8 +72,7 @@ namespace HumanResource.BackgroundWorkerFuncApp
                 employees = serializer.Deserialize<List<Employee>>(jtr);
             }
 
-            // Add individual employees to Service Bus Queue
-            Parallel.ForEach(employees, async e => await messages.AddAsync(e));
+            strategyAction(messages, employees);
 
             await messages.FlushAsync();
 
@@ -58,12 +81,24 @@ namespace HumanResource.BackgroundWorkerFuncApp
             var endTime = DateTime.UtcNow;
 
             // Write log information for analysis
-            var logline = $"\"{name}\",\"{blob.Length}\",\"{employees.Count}\",\"{stopwatch.ElapsedMilliseconds}\",\"{stopwatch.ElapsedTicks}\",\"{startTime:yyyy-MM-ddTH:mm:ss}\",\"{endTime:yyyy-MM-ddTH:mm:ss}\"";
+            var logLine =
+                $"\"{name}\",\"{blob.Length}\",\"{employees.Count}\",\"{stopwatch.ElapsedMilliseconds}\",\"{stopwatch.ElapsedTicks}\",\"{startTime:yyyy-MM-ddTH:mm:ss}\",\"{endTime:yyyy-MM-ddTH:mm:ss}\"";
 
-            using var sw = new StreamWriter(logStream, Encoding.UTF8);
-            await sw.WriteAsync(logline);
+            await using var sw = new StreamWriter(logStream, Encoding.UTF8);
+            await sw.WriteAsync(logLine);
             await sw.FlushAsync();
+        }
 
+        private static void ParallelAsyncProcessing(IAsyncCollector<Employee> messages, List<Employee> employees)
+        {
+            // Add individual employees to Service Bus Queue
+            Parallel.ForEach(employees, async e => await messages.AddAsync(e));
+        }
+
+        private static void AsyncProcessing(IAsyncCollector<Employee> messages, List<Employee> employees)
+        {
+            var tasks = employees.Select(employee => messages.AddAsync(employee)).ToArray();
+            Task.WaitAll(tasks);
         }
     }
 }
